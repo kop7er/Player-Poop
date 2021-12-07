c008d1bf56b987a36afd13349e9fda1c54f53256
Kopter <58952825+Kop7er@users.noreply.github.com>
2021-12-07 22:55:07 +0000

    Version: 2.0.0

--- PlayerPoop.cs
+++ PlayerPoop.cs
@@ -1,9 +1,12 @@
 ﻿using Newtonsoft.Json;
+
+using System.Collections.Generic;
+
 using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Player Poop", "Kopter", "1.1.2")]
+    [Info("Player Poop", "Kopter", "2.0.0")]
     [Description("Makes A Player Poop After Eating Food")]
 
     public class PlayerPoop : RustPlugin
@@ -11,10 +14,17 @@ namespace Oxide.Plugins
         #region Variables
 
         private int poopingProbability = 50;
-        
+
+        private const string horseDungShortname = "horsedung";
+
         private const string ignorePermission = "playerpoop.ignore";
         private const string canPoopPermission = "playerpoop.canpoop";
-        private const string horseDungShortname = "horsedung";
+
+        private const string screamSound = "assets/bundled/prefabs/fx/player/beartrap_scream.prefab";
+
+        private const string invisibleChairPrefab = "assets/bundled/prefabs/static/chair.invisible.static.prefab";
+
+        private readonly List<BaseMountable> spawnedChairs = new List<BaseMountable> { };
 
         #endregion
 
@@ -42,14 +52,51 @@ namespace Oxide.Plugins
             BasePlayer player = container.GetOwnerPlayer();
 
             if (player == null || ((player.metabolism.calories.value != player.metabolism.calories.max) && config.maxFullBar) || permission.UserHasPermission(player.UserIDString, ignorePermission) || (config.requiresPermission && !permission.UserHasPermission(player.UserIDString, canPoopPermission))) return;
-
+            
             Item horseDung = ItemManager.CreateByName(horseDungShortname);
 
             if (horseDung == null) return;
 
             horseDung.name = "Human Poop";
 
-            horseDung.Drop(player.transform.position, player.GetDropVelocity());
+            if (config.playScreamSound) Effect.server.Run(screamSound, player.transform.position);
+
+            if (config.sitPlayer && !(player.isMounted || player.IsFlying || player.IsSwimming()))
+            {
+                BaseMountable invisibleChair = GameManager.server.CreateEntity(invisibleChairPrefab, player.transform.position, player.transform.rotation) as BaseMountable;
+
+                if (invisibleChair == null)
+                {
+                    horseDung.Drop(player.transform.position, player.GetDropVelocity());
+                    return;
+                }
+
+                invisibleChair.Spawn();
+
+                spawnedChairs.Add(invisibleChair);
+
+                player.MountObject(invisibleChair);
+
+                timer.Once(3f, () => {
+
+                    invisibleChair?.Kill();
+
+                    spawnedChairs.Remove(invisibleChair);
+
+                    horseDung.Drop(player.transform.position, player.GetDropVelocity());
+
+                });
+            }
+
+            else horseDung.Drop(player.transform.position, player.GetDropVelocity());
+        }
+
+        private void Unload()
+        {
+            foreach (var chair in spawnedChairs)
+                chair?.Kill();
+
+            spawnedChairs.Clear();
         }
 
         #endregion
@@ -62,12 +109,18 @@ namespace Oxide.Plugins
         {
             [JsonProperty(PropertyName = "Requires Permission To Poop")]
             public bool requiresPermission = false;
-            
+
             [JsonProperty(PropertyName = "Probability Of Pooping (1-100)")]
             public int probabilityOfPooping = 50;
 
             [JsonProperty(PropertyName = "Only Poop If The Food Bar Is Full")]
             public bool maxFullBar = false;
+
+            [JsonProperty(PropertyName = "Sit Player When Pooping (3 Seconds)")]
+            public bool sitPlayer = false;
+
+            [JsonProperty(PropertyName = "Play Scream Sound When Pooping")]
+            public bool playScreamSound = false;
         }
 
         protected override void LoadConfig()
--- README.md
+++ README.md
@@ -12,10 +12,12 @@ Makes a player poop after eating food
 {
   "Requires Permission To Poop": false,
   "Probability Of Pooping (1-100)": 50,
-  "Only Poop If The Food Bar Is Full": false
+  "Only Poop If The Food Bar Is Full": false,
+  "Sit Player When Pooping (3 Seconds)": false,
+  "Play Scream Sound When Pooping": false
 }
 ```
 
 ## Example Video
 
-https://user-images.githubusercontent.com/58952825/132067618-6a019090-8824-4693-a806-2386493fa93a.mp4
+https://youtu.be/rN-UaKcnWxM
